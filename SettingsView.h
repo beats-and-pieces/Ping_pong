554d14314ffd08429b50e2c6ea039fed36e6cb84
Anton <Anton@antons-macbook-pro.local>
2019-04-17 12:01:39 +0300

    sadly

---
+++ SettingsView.h
@@ -0,0 +1,17 @@
+//
+//  SettingsView.h
+//  PingPong
+//
+//  Created by Anton Kuznetsov on 17/04/2019.
+//  Copyright © 2019 Anton Kuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SettingsView.m
@@ -0,0 +1,21 @@
+//
+//  SettingsView.m
+//  PingPong
+//
+//  Created by Anton Kuznetsov on 17/04/2019.
+//  Copyright © 2019 Anton Kuznetsov. All rights reserved.
+//
+
+#import "SettingsView.h"
+
+@implementation SettingsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
